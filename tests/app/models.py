d8ea8abeec01a0711b3df7c2dae6901359992be7
Oleg Nykolyn <juravel2@gmail.com>
2018-05-06 17:04:32 +0300

    Update test models.

--- tests/app/models.py
+++ tests/app/models.py
@@ -11,15 +11,17 @@ class FilmCategory(models.Model):
     id = models.AutoField('Identifier', primary_key=True)
     name = models.CharField('Name', max_length=70)
     parent_category = models.ForeignKey('FilmCategory',
-                                        null=True, default=None)
+                                        null=True,
+                                        default=None,
+                                        on_delete=models.PROTECT)
 
 
 class Film(models.Model):
     id = models.AutoField('Identifier', primary_key=True)
     year = models.IntegerField('Year')
     name = models.CharField('Name', max_length=70)
-    uploaded_by = models.ForeignKey(User)
-    category = models.ForeignKey(FilmCategory)
+    uploaded_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    category = models.ForeignKey(FilmCategory, on_delete=models.PROTECT)
 
 
 class CategoryHierarchy:
