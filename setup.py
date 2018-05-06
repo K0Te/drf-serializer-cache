db87efed65fbb53fe4973ff554dfee2396904860
Oleg Nykolyn <juravel2@gmail.com>
2018-05-06 17:10:05 +0300

    Update developement status.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/K0Te/drf-serializer-cache',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Framework :: Django',
         'License :: OSI Approved :: BSD License',
