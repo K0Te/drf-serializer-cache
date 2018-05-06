4b8ce3ad08ccca2fb4f7993618788b2d21c616cc
Oleg Nykolyn <juravel2@gmail.com>
2018-05-06 17:11:19 +0300

    Update version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='drf-serializer-cache',
-    version='0.2',
+    version='0.3.1',
     description='Django REST framework (DRF) serializer speedup',
     license='BSD',
     long_description=long_description,
